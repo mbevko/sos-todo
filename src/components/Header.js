883d217e8bd71ab938d5ba7dcbf1635639a68578
mbevko <mbevko@gmail.com>
2021-11-24 00:06:15 -0500

    styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ export default function Header ({list}) {
 
     return(
         <div className="header">
-            <h1>Reminders <span style={{color:"green"}}>{list.length}</span></h1>
+            <h1>Reminders</h1>
+            <h1 style={{color:"green"}}>{list.length}</h1>
         </div>
     )
 }
